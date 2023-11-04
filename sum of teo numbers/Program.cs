a86f3d5d918b7472e43c444fdf096ef4756ef153
user <user@DESKTOP-Q7SRQOE>
2023-11-04 21:42:45 +0200

    Add project files.

---
+++ sum of teo numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+int magic = int.Parse(Console.ReadLine());
+int count = 0;
+bool isFound = false;
+
+for (int x = start; x <= end; x++)
+{
+    for (int y = start ; y <= end; y++)
+    { 
+        count++;
+    int result = x + y;
+        if (result == magic)
+        {
+            Console.WriteLine($"Combination N:{count} ({x} + {y} = {magic})");
+            isFound = true;
+            break;
+        }
+    }
+    if(isFound)
+    {
+        break;
+    }
+
+}
+if (!isFound)
+{
+    Console.WriteLine($"{count} combinations - neither equals {magic}");
+}
+
---
+++ traveling Ani/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+double budget;
+
+while (input != "End")
+{ 
+ budget = double.Parse(Console.ReadLine());
+
+    double money = 0;
+    while (money < budget)
+    {
+        money += double.Parse(Console.ReadLine());
+    }
+
+
+
+    Console.WriteLine($"Going to {input}!");
+
+    input = Console.ReadLine();
+}
